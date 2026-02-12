c95f5a84b8368d1cc58ecd4e8fdc45f22b9ffa56
andrii <25188+unorsk@users.noreply.github.com>
2026-02-12 12:41:52 +0100

    adding haskeline so it's at least a little bit nicer from the start

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,21 @@
 
 module Main (main) where
 
+import Data.Text (pack)
 import Friskefrosk.Parse (parse)
+import System.Console.Haskeline
 import Text.Megaparsec.Error (errorBundlePretty)
 
 main :: IO ()
-main = do
-  let source = "<repl>"
-  case parse source "Parent(1, 2)." of
-    Left e -> putStr $ errorBundlePretty e
-    Right rules -> print rules
+main = runInputT defaultSettings loop
+  where
+    loop :: InputT IO ()
+    loop = do
+      minput <- getInputLine "frosk> "
+      case minput of
+        Nothing -> pure ()
+        Just input -> do
+          case parse "<repl>" (pack input) of
+            Left e -> outputStrLn $ errorBundlePretty e
+            Right rules -> outputStrLn $ show rules
+          loop
